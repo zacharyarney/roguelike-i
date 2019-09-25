d20e12fdebd6920986ec1c102d782e6d5e9e63e3
zacharyarney <zachary.arney@gmail.com>
2019-09-25 16:56:26 -0700

    wires up PlayScreen handleInput method

--- assets/screens/PlayScreen.js
+++ assets/screens/PlayScreen.js
@@ -9,15 +9,14 @@ class PlayScreen {
     display.drawText(3, 5, '%c{red}%b{white}This game is so much fun!');
     display.drawText(4, 6, 'Press [Enter] to win, or [Esc] to lose!');
   };
-  handleInput(inputType, inputData) {
+  handleInput(inputType, inputData, screenController) {
     if (inputType === 'keydown') {
       // If enter is pressed, go to the win screen
       // If escape is pressed, go to lose screen
-      if (inputData.keyCode === ROT.VK_RETURN) {
-        // this.switchScreen(this.winScreen);
-        return this.winScreen;
-      } else if (inputData.keyCode === ROT.VK_ESCAPE) {
-        this.switchScreen(this.loseScreen);
+      if (inputData.keyCode === 13 /* ROT.VK_RETURN */ ) {
+        screenController.switchScreen(screenController.winScreen);
+      } else if (inputData.keyCode === 27 /* ROT.VK_ESCAPE */ ) {
+        screenController.switchScreen(screenController.loseScreen);
       }
     }
   };
